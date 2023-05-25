14bcb2f1dfecc8cdf3e280556e8b9aadab759994
Dannan21 <daviseidel.brandao@gmail.com>
2023-05-25 12:34:51 -0300

    Update (broken)

--- cmd/update.go
+++ cmd/update.go
@@ -17,13 +17,9 @@ import (
 // updateCmd represents the update command
 var updateCmd = &cobra.Command{
 	Use:   "update",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Updates all AUR packages",
+	Long: `gets all AUR packages to the latest version. The syntax is:
+gaur update`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("update called")
 		var tempPath = os.TempDir()
@@ -57,7 +53,7 @@ to quickly create a Cobra application.`,
 				}
 				_ = exec.Command("sh", "-c", "cd "+tempPath+" && makepkg --printsrcinfo > "+tempPath+"/.SRCINFO")
 				//outSRCINFO, err := getSRCINFO.CombinedOutput()
-				//Teste git
+				//Teste gitname
 
 				if err != nil {
 					fmt.Println(err)
